5a3b6942a583fa98410e6194836a6b5200c2c96a
lucasclemerson <clemerson.lucas.oliveira@gmail.com>
2022-06-04 10:41:54 -0300

    Código para ocultar a lista de dados

---
+++ main.js
@@ -0,0 +1,19 @@
+function filter() {
+    var input, filter, ul, li, a, i;
+    input = document.getElementsByName("campo-busca")[0];
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("lista");
+    li = ul.getElementsByTagName("li");
+
+
+    for (i = 0; i < li.length; i++) {
+        a = li[i].getElementsByTagName("a")[0];
+        if (!input.value) {
+            li[i].style.display = "none";
+        } else if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "block";
+        } else {
+            li[i].style.display = "none";
+        }
+    }
+}
